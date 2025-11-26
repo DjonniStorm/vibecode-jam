5fc97c507009c43dcb2a246cb7bf40f9b9f74e9d
damirmagdeev <damirmagdeev1054@gmail.com>
2025-11-26 17:55:31 +0400

    init-backend

---
+++ backend/jam-backend/src/main/java/com/example/jam_backend/JamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.jam_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JamBackendApplication.class, args);
+    }
+
+}
